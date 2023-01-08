5f31f2c5ef49d24c3ad9a06324ee0f5b4c305f94
Gustavo Oki <gustavooki.dev@gmail.com>
2023-01-08 00:26:47 -0300

    adicionando comunicados de erros

--- js/buscarVideo.js
+++ js/buscarVideo.js
@@ -3,7 +3,6 @@ import constroiCard from "./mostrarVideos.js";
 
 async function buscarVideo(evento) {
     evento.preventDefault();
-
     const dadosDePesquisa = document.querySelector("[data-pesquisa]").value;
     const busca = await conectaApi.buscaVideo(dadosDePesquisa);
 
@@ -11,11 +10,15 @@ async function buscarVideo(evento) {
 
     while (lista.firstChild) {
         lista.removeChild(lista.firstChild);
-    }
+    };
 
     busca.forEach(elemento => lista.appendChild(
         constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)));
-}
+
+    if (busca.length == 0) {
+        lista.innerHTML = `<h2 class="mensagem__titulo">Não existem vídeos com esse termo</h2>`
+    };
+};
 
 const botaoDePesquisa = document.querySelector("[data-botao-pesquisa]");
 
